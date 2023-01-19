fdfe1b20e963cff162a220cbcceb4c27358c1579
Nachiket Karambelkar <nkarambelkar@agilesde.com>
2023-01-19 21:19:25 +0530

    added more intents to data

--- data_service.py
+++ data_service.py
@@ -14,25 +14,65 @@ class DataService:
         }
     ]
 
-    ourData = {"intents": [
-        {"tag": "age",
-         "patterns": ["how old are you?"],
-         "responses": ["I am 2 years old and my birthday was yesterday"]
-         },
-        {"tag": "greeting",
-         "patterns": ["Hi", "Hello", "Hey"],
-         "responses": ["Hi there", "Hello", "Hi :)"],
-         },
-        {"tag": "goodbye",
-         "patterns": ["bye", "later"],
-         "responses": ["Bye", "take care"]
-         },
-        {"tag": "name",
-         "patterns": ["what's your name?", "who are you?"],
-         "responses": ["I have no name yet," "You can give me one, and I will appreciate it"]
-         }
+    ourData = {
+        "intents": [
+            {
+                "tag": "age",
+                "patterns": ["how old are you?"],
+                "responses": ["I am 2 years old and my birthday was yesterday"]
+            },
+            {
+                "tag": "goodbye",
+                "patterns": ["bye", "later"],
+                "responses": ["Bye", "take care"]
+            },
+            {
+                "tag": "name",
+                "patterns": ["what's your name?", "who are you?"],
+                "responses": ["I have no name yet," "You can give me one, and I will appreciate it"]
+            },
+            {
+                "tag": "greeting",
+                "patterns": ["Hi", "Hello", "Hey", "Hi there", "Hi are you there", "Am I audible ", "Good morning",
+                             "Good afternoon", ""],
+                "responses": ["Hi, Nice to have you in call. Am I audible to you?"]
+            },
+            {
+                "tag": "greeting_positive_response",
+                "patterns": ["Yes" "Yes, you are audible", "I checked settings you are audible now",
+                             "Yes you are", "Yes I can hear you"],
+                "responses": ["Amazing.. Can we get started? Are you ready for your interview?",
+                              "That's great, Are you ready for an interview?",
+                              "Good, we can get started then"]
+            },
+            {
+                "tag": "greeting_negative_response",
+                "patterns": ["No", "Nope", "No, you are not", "Nope, you are not",
+                             "No you are not audible", "Nope, you are not audible"],
+                "responses": ["Oh that's sad, Can you please check your network connection "]
+            },
+            {
+                "tag": "no_network_issue",
+                "patterns": ["Yes, I did check my connection. Its fine ",
+                             "Yes, I did check my connection. It is fine ",
+                             "I am not facing any network issue",
+                             "I do not have any issue with network",
+                             "I have strong connection"],
+                "responses": ["Oh, glad to hear that. Can you check settings once and let me know if you are audible?", ]
+            },
+            {
+                "tag": "introduction",
+                "patterns": ["My name is *", "I work in ", "I have experience of", "I am "],
+                "responses": ["That's great, Let's start with Java section."]
+            },
+            {
+                "tag": "after_greeting",
+                "patterns": ["Yes", "yes sure", "Yes definitely", "Yes, we can get started"],
+                "responses": ["Would you like to introduce yourself?"]
+            }
+            
 
-    ]}
+        ]}
 
     def get_data(self):
         return self.ourData
--- test_chatbot_model.py
+++ test_chatbot_model.py
@@ -5,11 +5,12 @@ from data_service import DataService
 
 def initiate():
     our_classes, new_words = build_model.build_ml_model()
-    new_message = input("")
-    intents = build_model.Pclass(new_message, new_words, our_classes)
-    data = DataService()
-    reply = build_model.getRes(intents, data.get_data())
-    print(reply)
+    while True:
+        new_message = input("")
+        intents = build_model.Pclass(new_message, new_words, our_classes)
+        data = DataService()
+        reply = build_model.getRes(intents, data.get_data())
+        print(reply)
 
 
 initiate()
